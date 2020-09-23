9c8c10efdbc14e5cc64ff51c766e980199f5ede1
endroni <rubao.rcs@gmail.com>
2020-09-23 15:37:19 -0300

    aula01 e .gitignore

---
+++ aula_01/aula_01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aula_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo!");
+        }
+    }
+}
