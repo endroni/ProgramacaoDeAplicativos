46f308e4ce9959fd9eb71648063f4a3ebd27c00d
endroni <rubao.rcs@gmail.com>
2020-09-30 15:21:13 -0300

    conversao de valores

---
+++ Aula_04_Dados/ConversaoValores/ConversaoValores/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ConversaoValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Conversão implícita entre tipos */
+
+            float x = 7.5f; /* Float - Tamanho de 4 bytes */
+
+            double y = x;    /* Double - Tamanho de 8 bytes */
+
+            Console.WriteLine(y);
+
+            /* 
+                Ocorreu uma conversão implícita de dados, 
+                porque y recebeu perfeitamente o valor de x,
+                é possível por causa do tamanho em bytes. 
+
+             */
+
+            double a;
+            float b;
+
+            a = 5.1;
+            b = (float)a;
+            Console.WriteLine(b);
+
+            /* 
+               Como double tem 8 bytes e float tem 4 bytes. 
+               O compilador avisa: Dados podem ser perdidos 
+               para isso precisamos fazer uma 
+               conversão explicita de tipos 
+             */
+
+            double c;
+            int d;
+            c = 6.1;
+            d = (int)c;
+
+            Console.WriteLine(d);
+            /* 
+                Nesse caso o compilador avisa que podemos 
+                perder dados por causa do tipo do dado 
+                
+                Valores truncados = Quando perdemos parte do dado
+            */
+
+            int e = 5;
+            int f = 2;
+
+            double resultado = (double)e / f;
+
+            Console.WriteLine(resultado);
+
+            /* Como os valores de e , f são inteiros. 
+               O compilador achou que eu queria uma divisão inteira. 
+               Para alterar o resultado. Precisamos fazer um casting
+
+               double resultado = (double) e / f ;
+            */
+        }
+    }
+}
---
+++ Aula_04_Dados/OperadoresDeAtribuição/OperadoresDeAtribuição/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace OperadoresDeAtribuição
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Operadores de atribuição 
+              Operador    Exemplo     Significado 
+                =       a = 10;         a RECEBE 10 
+                +=      a += 2;         a RECEBE a + 2;
+                -=      a -= 2;         a RECEBE a - 2;
+                *=      a *= 2;         a RECEBE a * 2;
+                /= a    /= 2;           a RECEBE a / 2;
+                %=      a %= 3;         a RECEBE a % 3;
+
+        Exemplo:
+            */
+            int a = 10;
+            Console.WriteLine(a);
+            a += 2;
+            Console.WriteLine(a);
+
+            a -= 2;
+            Console.WriteLine(a);
+
+            a *= 2;
+            Console.WriteLine(a);
+
+            a /= 2;
+            Console.WriteLine(a);
+
+            a %= 3;
+            Console.WriteLine(a);
+
+            /* Atribuição também funciona com sting */
+
+            string s = "ABC";
+            Console.WriteLine(s);
+
+            s += "DEF";
+            Console.WriteLine(s);
+
+            /* Operador aritméticos / atribuição
+                 Operador     Exemplo         Significado 
+                  ++          b++; ou ++b;    b = b + 1; 
+                  --          b--; ou --b;    b = b - 1; 
+
+                 Exemplo: */
+
+            int b = 10;
+            b++;
+            Console.WriteLine(b);
+
+            b--;
+            Console.WriteLine(b);
+
+            Console.WriteLine(); /* só para dar um espaço no código */
+
+            int c = 10;
+            int d = c++;            /* d recebeu c == 10 */
+            Console.WriteLine(c);   /* só depois c foi incrementado */
+            Console.WriteLine(d);
+
+            Console.WriteLine();
+
+            int e = 10;
+            int f = ++e;                /* e foi incrementado */
+            Console.WriteLine(e);       /* depois f recebeu o valor */
+            Console.WriteLine(f);
+        }
+    }
+}
