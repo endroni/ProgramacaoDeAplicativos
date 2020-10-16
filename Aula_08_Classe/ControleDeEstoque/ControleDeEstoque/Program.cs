7e6082aa72bc9f9423a99c6a429c89f3b8c6d4ee
endroni <rubao.rcs@gmail.com>
2020-10-16 16:38:23 -0300

    override, ToString, void

---
+++ Aula_08_Classe/ControleDeEstoque/ControleDeEstoque/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace ControleDeEstoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();  
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            double total = p.ValorTotalEmEstoque();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+
+            Console.WriteLine();
+
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Dados do produto: " + p);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Aula_08_Classe/Triangulo_OO/Triangulo_OO/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace Triangulo_OO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y; // Declara as variáveis
+
+            x = new Triangulo(); // Faz instanciação
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = x.Area();
+            Console.WriteLine("Area de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+
+            double areaY = y.Area();
+            Console.WriteLine("Area de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.Write("Maior area X");
+            }
+            else
+            {
+                Console.Write("Maior area Y");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
