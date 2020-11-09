198db18c25a3f76a9c5c654c63d5cdd747facd9e
endroni <rubao.rcs@gmail.com>
2020-11-09 16:15:35 -0300

    praticando this

---
+++ Aula_08_Classe/Exercícios/PraticandoThisEscola/Escola/Professor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Escola
+{
+    class Professor
+    {
+        public string Nome;
+        public string Sobrenome;
+        public int Telefone;
+        public Boolean Oculos;
+
+        public Professor()
+        {
+            Oculos = true;
+        }
+
+        public Professor(string nome, string sobrenome, int telefone) : this()
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Telefone = telefone;
+        }
+
+        public string UsaOculos(Boolean oculos)
+        {
+            if (oculos == true)
+            {
+                return "4 olhos";
+            } else
+            {
+                return "meio cego";
+            }
+        }
+
+        public override string ToString()
+        {
+            return "O professor " 
+                + Nome
+                + " "
+                + Sobrenome 
+                + ", telefone " 
+                + Telefone
+                + " é "
+                + UsaOculos(Oculos);
+        }
+    }
+}
---
+++ Aula_08_Classe/Exercícios/PraticandoThisEscola/Escola/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Escola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Rubem";
+            string sobrenome = "Cândido";
+            int tel = 987655;
+
+            Professor r = new Professor(nome, sobrenome, tel);
+            Console.WriteLine(r);
+        }
+    }
+}
