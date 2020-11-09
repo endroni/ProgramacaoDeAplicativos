23b89bc68f6e1f962efd7c28a3766ab657fb34de
endroni <rubao.rcs@gmail.com>
2020-11-09 16:22:00 -0300

    exercicio de fixação executado

---
+++ Aula_08_Classe/Exercícios/PraticandoThisEscola/Escola/Aluno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Escola
+{
+    class Aluno
+    {
+        public string Nome;
+        public string Email;
+        public string Cidade;
+        public Boolean Status;
+
+        public Aluno()
+        {
+            Status = true;
+        }
+
+        public Aluno (string nome, string email, string cidade) : this()
+        {
+            Nome = nome;
+            Email = email;
+            Cidade = cidade;
+        }
+    }
+}
