eed09d2248bd56edfb23a74de582dd7a6e05c34a
endroni <rubao.rcs@gmail.com>
2020-11-18 16:15:59 -0300

    propreties e auto propreties

---
+++ Aula_08_Classe/Propeties_Aula/Propeties_Aula/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace ControleDeEstoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV nome antigo", 500.00, 10);
+
+            p.Nome = "TV novo nome";
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+        }
+    }
+}
