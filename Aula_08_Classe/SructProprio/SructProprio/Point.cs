c307f90cf23a85ea0606e413213eeb03942c147e
endroni <rubao.rcs@gmail.com>
2020-11-18 16:03:40 -0300

    criando um struct - exemplo do plano cartesiano

---
+++ Aula_08_Classe/SructProprio/SructProprio/Point.cs
@@ -0,0 +1,14 @@
+﻿namespace SructProprio
+{
+    struct Point
+    {
+        public double X;
+        public double Y;
+
+        public override string ToString()
+        {
+            return "(" + X + ", " + Y + ")"; 
+        }
+    }
+}
+
