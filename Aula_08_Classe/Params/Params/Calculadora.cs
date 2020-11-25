e5b3bf6f85fe91966e76c70ed20e11fb21159420
endroni <rubao.rcs@gmail.com>
2020-11-25 10:45:55 -0300

    params - modificador de parâmetros

---
+++ Aula_08_Classe/Params/Params/Calculadora.cs
@@ -0,0 +1,16 @@
+﻿namespace Params
+{
+    class Calculadora
+    {
+        public static int Soma(params int[] numeros)
+        {
+            int soma = 0;
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                soma += numeros[i];
+            }
+            return soma;
+        }
+    }
+}
+
---
+++ Aula_08_Classe/Params/Params/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculadora.Soma( 2, 3 );
+            int s2 = Calculadora.Soma( 3, 4, 5 );
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+    }
+}
+
+
