36ac73c66f4a8a8c15ff3a5a78538b75a5e3ead4
endroni <rubao.rcs@gmail.com>
2020-11-25 08:28:58 -0300

    exercicio pensionista iniciado

---
+++ Aula_08_Classe/Exercícios/Pensionado/Pensionado/Cliente.cs
@@ -0,0 +1,14 @@
+﻿namespace Pensionado
+{
+    class Cliente
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Cliente (string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+    }
+}
---
+++ Aula_08_Classe/Exercícios/Pensionado/Pensionado/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Pensionado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Cliente[] cli = new Cliente[n];
+
+            for ( int i = 0; i < n; i++ )
+            {
+                Console.WriteLine("Aluguel #" + (i + 1) + ":");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                cli[quarto] = new Cliente(nome, email);
+            }
+
+            Console.WriteLine(cli[0].Nome);
+            
+        }
+    }
+}
