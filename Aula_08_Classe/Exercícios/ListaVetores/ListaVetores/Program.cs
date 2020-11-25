50d9b67a62bf20b9abaab8ba5f484d99fa53cf1d
endroni <rubao.rcs@gmail.com>
2020-11-25 11:20:33 -0300

    iniciado lista de vetores

---
+++ Aula_08_Classe/Exercícios/ListaVetores/ListaVetores/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ListaVetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Crie um programa que leia 6 valores inteiros e, em seguida, mostre na tela os valores lidos.
+
+            int[] vetor = new int[6];
+            for (int i = 0; i < 6; i++)
+            {
+                vetor[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+            for(int i = 0; i < 6; i++ )
+            {
+                Console.WriteLine(vetor[i]);
+            }
+        }
+    }
+}
