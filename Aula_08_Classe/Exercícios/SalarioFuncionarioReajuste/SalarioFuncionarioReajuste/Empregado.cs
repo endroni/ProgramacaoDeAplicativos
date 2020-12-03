5efefbbaea8e98b7cd3945ed2fc9d85f892dce11
endroni <rubao.rcs@gmail.com>
2020-12-03 16:31:48 -0300

    Empregados registro exercicio iniciado

---
+++ Aula_08_Classe/Exercícios/SalarioFuncionarioReajuste/SalarioFuncionarioReajuste/Empregado.cs
@@ -0,0 +1,11 @@
+﻿namespace SalarioFuncionarioReajuste
+{
+    class Empregado
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+
+    }
+}
