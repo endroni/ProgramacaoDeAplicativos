11f6da575b3b2645f6d692e8815057ba717b68a1
endroni <rubao.rcs@gmail.com>
2020-12-04 16:40:00 -0300

    Lista Orientada Objeto

---
+++ Aula_08_Classe/ListaPOO/ListaPOO/Esportes.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace ListaPOO
+{
+    class Esportes
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Investimento { get; set; }
+
+        public Esportes(int id, string nome, double investimento)
+        {
+            Id = id;
+            Nome = nome;
+            Investimento = investimento;
+        }
+
+        public override string ToString()
+        {
+            return "Id: "
+                + Id +
+                " : " +
+                Nome
+                + " R$ "
+                + Investimento.ToString("F2", CultureInfo.InvariantCulture)
+                ;
+        }
+    }
+}
---
+++ Aula_08_Classe/ListaPOO/ListaPOO/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListaPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos esportes você quer adicionar? ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Esportes> esporte = new List<Esportes>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Esporte # " + i + 1);
+                Console.Write("Digite o Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Digite o Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Investimento R$ ");
+                double investimento = double.Parse(Console.ReadLine());
+
+                esporte.Add(new Esportes(id, nome, investimento));
+            }
+            Console.Clear();
+
+            foreach (Esportes esp in esporte)
+            {
+                Console.WriteLine(esp);
+            }
+        }
+    }
+}
