3d0324eebc3a36ca68af0a1ec32474d435346e07
endroni <rubao.rcs@gmail.com>
2020-12-04 16:09:30 -0300

    lista animais aula exemplo

---
+++ Aula_08_Classe/ListasAula/ListasAula/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListasAula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> animais = new List<string>();
+
+            animais.Add("Vaca");
+            animais.Add("Cão");
+            animais.Add("Gato");
+            animais.Add("Cabra");
+
+            animais.Insert(0, "Peixe");
+
+            foreach(string zoo in animais)
+            {
+                Console.WriteLine(zoo);
+            }
+
+            Console.WriteLine("Essa lista tem " + animais.Count + " animais");
+
+            animais.RemoveRange(1, 3);
+            
+            Console.WriteLine("-----REMOVENDO------");
+            foreach (string zoo in animais)
+            {
+                Console.WriteLine(zoo);
+            }
+
+        }
+    }
+}
