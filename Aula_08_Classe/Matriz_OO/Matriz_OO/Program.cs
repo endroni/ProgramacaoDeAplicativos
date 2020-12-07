ec08b95674953924beb66e72f75baea769ef2985
endroni <rubao.rcs@gmail.com>
2020-12-07 13:58:23 -0300

    matriz teoria iniciado

---
+++ Aula_08_Classe/Matriz_OO/Matriz_OO/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Matriz_OO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] matriz = new double[2, 3];
+            
+            Console.WriteLine(matriz.Length);
+            Console.WriteLine(matriz.Rank);
+            Console.WriteLine(matriz.GetLength(0));
+            Console.WriteLine(matriz.GetLength(1));
+        }
+    }
+}
+
