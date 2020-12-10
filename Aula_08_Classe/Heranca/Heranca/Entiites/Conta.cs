5bf37c901fc9aed1bc056952733f8c177599856a
endroni <rubao.rcs@gmail.com>
2020-12-10 10:25:36 -0300

    classe Conta implementada no projeto Heranca

--- Aula_08_Classe/Heranca/Heranca/Entiites/Conta.cs
+++ Aula_08_Classe/Heranca/Heranca/Entiites/Conta.cs
@@ -6,6 +6,26 @@
         public string Titular { get; set; }
         public double Saldo { get; set; }
 
+        public Conta()
+        {
+        }
+
+        public Conta(int numero, string titular, double saldo)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = saldo;
+        }
+
+        public void Saque(double valor)
+        {
+            Saldo -= valor;
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
     }
 }
 
