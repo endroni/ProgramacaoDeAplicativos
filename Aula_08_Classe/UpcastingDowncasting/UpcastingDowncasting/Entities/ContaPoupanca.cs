6d7491670c638b291c0de5a6e36754696826e40d
endroni <rubao.rcs@gmail.com>
2020-12-11 10:26:33 -0300

    upcasting testes

--- Aula_08_Classe/UpcastingDowncasting/UpcastingDowncasting/Entities/ContaPoupanca.cs
+++ Aula_08_Classe/UpcastingDowncasting/UpcastingDowncasting/Entities/ContaPoupanca.cs
@@ -1,7 +1,23 @@
 ﻿namespace UpcastingDowncasting.Entities
 {
-    class ContaPoupanca
+    class ContaPoupanca : Conta
     {
+        public double TaxaJuros { get; set; }   
+
+        public ContaPoupanca()
+        {
+        }
+
+        public ContaPoupanca(int numero, string titular, double saldo, double taxaJuros)
+            : base(numero, titular, saldo)
+        {
+            TaxaJuros = taxaJuros;
+        }
+
+        public void AtualizaSaldo()
+        {
+            Saldo += Saldo * TaxaJuros;
+        }
     }
 }
 
--- Aula_08_Classe/UpcastingDowncasting/UpcastingDowncasting/Program.cs
+++ Aula_08_Classe/UpcastingDowncasting/UpcastingDowncasting/Program.cs
@@ -7,6 +7,14 @@ namespace UpcastingDowncasting
     {
         static void Main(string[] args)
         {
+            Conta conta = new Conta(101, "Jiraya", 0.0);
+            ContaEmpresa empresa = new ContaEmpresa(1001, "Kurama", 0.0, 400.0);
+
+            //UPCASTING 
+            Conta conta2 = empresa;
+            Conta conta3 = new ContaEmpresa(1002, "Kakashi", 0.0, 300.0);
+            Conta conta4 = new ContaPoupanca(2001, "Hinata", 0.0, 0.01);
         }
     }
 }
+
