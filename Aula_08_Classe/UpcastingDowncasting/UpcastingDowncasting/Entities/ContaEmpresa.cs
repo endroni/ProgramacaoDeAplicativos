ae9117d8b78020325232de513c99a5e5633bd3e5
endroni <rubao.rcs@gmail.com>
2020-12-11 09:39:30 -0300

    Upcasting e Downcasting iniciado

---
+++ Aula_08_Classe/UpcastingDowncasting/UpcastingDowncasting/Entities/ContaEmpresa.cs
@@ -0,0 +1,26 @@
+﻿namespace UpcastingDowncasting.Entities
+{
+    class ContaEmpresa : Conta
+    {
+        public double LimiteEmprestimo { get; set; }
+
+        public ContaEmpresa()
+        {
+        }
+
+        public ContaEmpresa(int numero, string titular, double saldo, double limiteEmprestimo)
+            : base(numero, titular, saldo)
+        {
+            LimiteEmprestimo = limiteEmprestimo;
+        }
+
+        public void Emprestimo(double valor)
+        {
+            if (valor <= LimiteEmprestimo)
+            {
+                Saldo += valor;
+            }
+        }
+    }
+}
+
---
+++ Aula_08_Classe/UpcastingDowncasting/UpcastingDowncasting/Entities/ContaPoupanca.cs
@@ -0,0 +1,8 @@
+﻿namespace UpcastingDowncasting.Entities
+{
+    class ContaPoupanca
+    {
+    }
+}
+
+
