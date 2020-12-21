52acdb88ca2894f2f986da4f4831e482574f8240
endroni <rubao.rcs@gmail.com>
2020-12-21 12:48:54 -0300

    exercicio

---
+++ Aula_08_Classe/Exercícios/Avaliacao/Avaliacao/Entities/Carro.cs
@@ -0,0 +1,26 @@
+﻿namespace Avaliacao.Entities
+{
+    class Carro
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int AnoFabricacao { get; set; }
+
+        public Carro()
+        {
+        }
+
+        public Carro(string marca, string modelo, int anoFabricacao)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            AnoFabricacao = anoFabricacao;
+        }
+
+        public override string ToString()
+        {
+            return Modelo + " ano " + AnoFabricacao;
+        }
+    }
+}
+
---
+++ Aula_08_Classe/Exercícios/Avaliacao/Avaliacao/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Avaliacao.Entities;
+
+namespace Avaliacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro1 = new Carro("Volkswagem", "Passate", 1983);
+
+            Console.WriteLine("Meu primeiro carro foi um " + carro1);
+        }
+    }
+}
+
